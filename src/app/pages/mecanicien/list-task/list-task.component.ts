8119a4f893bfb39de5416d9a451c1fa674185c33
Miandritiana <120574096+Miandritiana@users.noreply.github.com>
2025-04-23 18:47:48 +0300

    Add recherche mecanicien tache

--- src/app/pages/mecanicien/list-task/list-task.component.ts
+++ src/app/pages/mecanicien/list-task/list-task.component.ts
@@ -15,13 +15,16 @@ import {
   TextColorDirective,
   ModalModule,
   FormCheckComponent,
+  DropdownModule
 }
 from '@coreui/angular';
+import { FormsModule } from '@angular/forms';
 
 import { NgFor, NgStyle, NgIf, NgClass, CommonModule } from '@angular/common';
 import { MecanicienService } from '../../../services/mecanicien.service';
 import { LocalStorageService } from '../../../services/local-storage.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-task',
@@ -41,7 +44,9 @@ import { Router } from '@angular/router';
     TextColorDirective,
     ModalModule,
     FormCheckComponent,
-    NgFor, NgStyle, NgIf, NgClass, CommonModule
+    NgFor, NgStyle, NgIf, NgClass, CommonModule,
+    DropdownModule,
+    FormsModule
   ],
   templateUrl: './list-task.component.html',
   styleUrl: './list-task.component.scss'
@@ -51,8 +56,11 @@ export class ListTaskComponent implements OnInit{
   listTache: any[] = [];
   @Output() dataEvent = new EventEmitter<any>();
   isAppear: boolean = true;
-
-
+  selectedDate: string = '';
+  selectedAvancement: string = '';
+  selectedAvancementNumber: number = 0;
+  selectedDateFormated: Date | null = null;
+  avancementList: any = [0, 1, 2, 3];
 
   constructor (
     private mecanicienService: MecanicienService,
@@ -76,6 +84,8 @@ export class ListTaskComponent implements OnInit{
 
   getBadgeClass(avancement: number): string {
     switch (avancement) {
+      case 0:
+        return 'bg-info';
       case 1:
         return 'bg-warning'; // En attente (yellow)
       case 2:
@@ -89,6 +99,8 @@ export class ListTaskComponent implements OnInit{
 
   getBadgeLabel(avancement: number): string {
     switch (avancement) {
+      case 0:
+        return 'Tous';
       case 1:
         return 'En attente';
       case 2:
@@ -100,6 +112,13 @@ export class ListTaskComponent implements OnInit{
     }
   }
 
+
+  selectAvancement(item: any) {
+    console.log(item);
+    this.selectedAvancement = item;
+    this.selectedAvancementNumber = Number(this.selectedAvancement);
+  }
+
   goDetail(idrendezvous: string) {
     // this.isAppear = false;
     this.dataEvent.emit(idrendezvous);
@@ -108,5 +127,35 @@ export class ListTaskComponent implements OnInit{
 
   reload() {
     this.getListTache(this.localStorageService.getLoginInfo()?.iduser ?? '');
+  }
+
+
+  appliquer() {
+    var idmecanicien = this.localStorageService.getLoginInfo()?.iduser ?? '';
+    const dateObject = new Date(this.selectedDate);
+    this.selectedDateFormated = new Date(Date.UTC(
+      dateObject.getFullYear(),
+      dateObject.getMonth(),
+      dateObject.getDate(),
+      dateObject.getHours(),
+      dateObject.getMinutes()
+    ));
+
+    let avancementParam = this.selectedAvancementNumber === 0 ? undefined : this.selectedAvancementNumber;
+    this.mecanicienService.tacheWithSearch(this.selectedDateFormated, avancementParam, idmecanicien).subscribe(
+      data => {
+        this.listTache = data;
+      },
+      (error) => {
+        console.error('Erreur lors du chargement des rendez-vous', error);
+        const errorMessage = error?.error?.message || 'Une erreur est survenue';
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Erreur',
+          text: errorMessage,
+        });
+      }
+    )
   }
 }
